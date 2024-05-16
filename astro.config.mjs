55675446aa9e22b13c27647ab5b59de11c11e818
ethqnol <112029223+ethqnol@users.noreply.github.com>
2024-05-15 21:11:14 -0400

    s

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import rehypeMathjax from 'rehype-mathjax';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ethqnol.github.io',
-  base: '/',
+  base: './',
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeMathjax],
